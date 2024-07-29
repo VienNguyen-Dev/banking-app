05829d196b0c5cd37a9312f11c0a4b9afdfef05e
VienNguyen-Dev <149781283+VienNguyen-Dev@users.noreply.github.com>
2024-07-29 09:00:05 +0700

    Create HeaderBox, TotalBalanceBox and doughnut Component for the homepage

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,7 +1,19 @@
+import HeaderBox from "@/components/HeaderBox";
+import TotalBalanceBox from "@/components/TotalBalanceBox";
 import React from "react";
 
 const Home = () => {
-  return <div></div>;
+  const loggedIn = { firstName: "VienNguyen" };
+  return (
+    <section className="home">
+      <div className="home-content">
+        <header className="home-header">
+          <HeaderBox type="greeting" title={`Wellcome`} user={loggedIn.firstName} subtext="Access & manage your account and transactions efficiently." />
+        </header>
+        <TotalBalanceBox accounts={[]} totalBanks={1} totalCurrentBalance={2698.12} />
+      </div>
+    </section>
+  );
 };
 
 export default Home;
