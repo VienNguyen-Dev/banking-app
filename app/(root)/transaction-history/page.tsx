b8358012e63050fbdf6a5eb5475c6ac634a9a2d6
VienNguyen-Dev <149781283+VienNguyen-Dev@users.noreply.github.com>
2024-08-19 10:05:00 +0700

    add select account bank in the transaction history page

--- app/(root)/transaction-history/page.tsx
+++ app/(root)/transaction-history/page.tsx
@@ -1,22 +1,30 @@
+import BankDropdown from "@/components/BankDropdown";
 import HeaderBox from "@/components/HeaderBox";
+import Pagination from "@/components/Pagination";
 import TransactionTable from "@/components/TransactionTable";
 import { getAccount, getAccounts } from "@/lib/actions/bank.actions";
 import { getLoggedInUser } from "@/lib/actions/user.actions";
 import { formatAmount } from "@/lib/utils";
 import React from "react";
 
-const TransactionHistory = async ({ searchParams: { id } }: SearchParamProps) => {
+const TransactionHistory = async ({ searchParams: { id, page } }: SearchParamProps) => {
   const loggedIn = await getLoggedInUser();
   const accounts = await getAccounts({ userId: loggedIn.$id });
   const appwriteItemId = (id as string) || accounts.data[0].appwriteItemId;
   const account = await getAccount({ appwriteItemId });
+  const currentPage = Number(page as string) || 1;
+  const indexOfLastTransaction = currentPage * 10;
+  const indexOfStartTransaction = (currentPage - 1) * 10;
+  const currentTransactions = account.allTransactions.slice(indexOfStartTransaction, indexOfLastTransaction);
+  const totalPages = Math.ceil(account.allTransactions.length / 10);
   //Handle change bank here
 
   return (
     <div className="flex flex-col gap-4 transactions">
-      <div className="flex justify-between">
+      <div className="transactions-header">
         <HeaderBox type="title" title="Transaction history" subtext="Gain insights and track your transactions over time" />
         {/* Select Account */}
+        <BankDropdown accounts={accounts.data} />
       </div>
       <div className=" space-y-4">
         <div className="transactions-account">
@@ -32,7 +40,8 @@ const TransactionHistory = async ({ searchParams: { id } }: SearchParamProps) =>
             <p className="text-[24px] font-bold">{formatAmount(account.data.currentBalance)}</p>
           </div>
         </div>
-        <TransactionTable transactions={account.allTransactions} />
+        <TransactionTable transactions={currentTransactions} />
+        {totalPages > 1 && <Pagination totalPages={totalPages} page={currentPage} />}
       </div>
     </div>
   );
--- components/RecentTransactions.tsx
+++ components/RecentTransactions.tsx
@@ -1,13 +1,17 @@
 import Link from "next/link";
-import React from "react";
 import { Button } from "./ui/button";
-import { cn } from "@/lib/utils";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import BankTabItem from "./BankTabItem";
 import BankInfo from "./BankInfo";
 import TransactionTable from "./TransactionTable";
+import Pagination from "./Pagination";
 
 const RecentTransactions = ({ accounts = [], transactions = [], appwriteItemId, page }: RecentTransactionsProps) => {
+  const rowPerTrans = 10;
+  const totalPages = Math.ceil(transactions.length / rowPerTrans);
+  const indexOfFirstTransaction = (page - 1) * rowPerTrans;
+  const indexOfLastTransaction = page * rowPerTrans;
+  const currentTransactions = transactions.slice(indexOfFirstTransaction, indexOfLastTransaction);
   return (
     <div className="recent-transactions">
       <header className="flex flex-col gap-4">
@@ -33,12 +37,13 @@ const RecentTransactions = ({ accounts = [], transactions = [], appwriteItemId, 
             <div className="flex flex-col gap-4" key={a.id}>
               <TabsContent value={a.appwriteItemId}>
                 <BankInfo account={a} appwriteItemId={a.appwriteItemId} type="full" />
-                <TransactionTable transactions={transactions} />
+                <TransactionTable transactions={currentTransactions} />
               </TabsContent>
             </div>
           ))}
         </Tabs>
       </header>
+      <Pagination page={page} totalPages={totalPages} />
     </div>
   );
 };
